c7f5f1790d3780f455667e72535380512bf50a9a
suhaibzahmed <suhaib.z.ahmed@gmail.com>
2025-07-03 12:18:11 +0530

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,10 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client'],
-  },
+  serverExternalPackages: ['@prisma/client'],
+  // experimental: {
+  //   serverComponentsExternalPackages: ['@prisma/client'],
+  // },
 }
 
 export default nextConfig
